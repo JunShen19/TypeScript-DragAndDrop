0e7c75e3ccf90d20936cc04cebf763be97a00970
JunShen <tony88010955@gmail.com>
2023-08-03 14:28:01 +0800

    manage file with namesapces

---
+++ src/decorators/autobind.ts
@@ -0,0 +1,19 @@
+namespace App {
+  // autobind decorator
+  export function autobind(
+    _: any,
+    _2: string,
+    description: PropertyDescriptor
+  ) {
+    const originalMethod = description.value;
+    const adjDescriptor: PropertyDescriptor = {
+      configurable: true,
+      get() {
+        const boundFn = originalMethod.bind(this);
+        return boundFn;
+      },
+    };
+
+    return adjDescriptor;
+  }
+}
